ed3e68530ad87246463aa4754029dfac875da63d
Xavier Lopez <xlopezmin@gmail.com>
2023-11-14 11:21:06 +0100

    feat(log): middleware

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const logMiddleware = require('../middlewares/log');
 const todoRouter = require('./todo');
 
 const router = express.Router();
 
+router.use('/', logMiddleware.addLog);
 router.use('/todo', todoRouter);
 
 module.exports = router;
