7ec9a1de8a11b611ec1a6cab59690ccab66de6de
Xavier Lopez <xlopezmin@gmail.com>
2023-11-24 12:26:39 +0100

    chore(todo): add task in todoData

--- src/data/index.js
+++ src/data/index.js
@@ -1,12 +1,49 @@
 exports.todos = [{
   id: 0,
-  text: "Tarea 0",
-  fecha: "2023-11-01",
-  done: false
+  text: "TD-1: [API] APIRest ToDo List",
+  fecha: "2023-11-20",
+  done: true
 },
 { 
   id: 1,
-  text: "Tarea 1",
-  fecha: "2023-11-02",
+  text: "TD-2: [UI] Task Add",
+  fecha: "2023-11-21",
   done: true
-}];
+},
+{ 
+  id: 2,
+  text: "TD-3: [UI] Task List",
+  fecha: "2023-11-22",
+  done: true
+},
+{ 
+  id: 3,
+  text: "TD-4: [UI] Task Detail",
+  fecha: "2023-11-23",
+  done: true
+},
+{ 
+  id: 4,
+  text: "TD-5: [UI] Routes",
+  fecha: "2023-11-23",
+  done: true
+},
+{ 
+  id: 5,
+  text: "TD-6: [UI] Look & Feel",
+  fecha: "2023-11-24",
+  done: true
+},
+{ 
+  id: 6,
+  text: "TD-7: [API] getAll with queryParams",
+  fecha: "2023-11-24",
+  done: true
+},
+{ 
+  id: 7,
+  text: "TD-8: [UI] get task by state",
+  fecha: "2023-11-24",
+  done: false
+}
+];
